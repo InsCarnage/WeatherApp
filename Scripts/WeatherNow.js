f6ab8374e37eff1edd8c92975538e974f2d950cb
Jeandre van Zyl <47389684+InsCarnage@users.noreply.github.com>
2021-01-04 01:08:04 +0200

    Update

--- Scripts/WeatherNow.js
+++ Scripts/WeatherNow.js
@@ -1,51 +1,68 @@
 
+const Joh = 'https://api.openweathermap.org/data/2.5/weather?q=Johannesburg&appid=2f5c5e465bbf3515e51929ac50b00522&units=metric';
+const Cape = 'https://api.openweathermap.org/data/2.5/weather?q=Cape Town&appid=2f5c5e465bbf3515e51929ac50b00522&units=metric';
 
 
 
 
 
+getAPI();
+getAPI2();
 
-getLocation();
 
-function getLocation() {
-    if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-    } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
 
-function showPosition(position) {
-    document.getElementById("demo").innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude;
-    getAPI(position.coords.latitude, position.coords.longitude);
-}
-
-async function getAPI(Lat, Long){
-    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat="+Lat+"&lon="+Long+"&appid=2f5c5e465bbf3515e51929ac50b00522&units=metric");
+async function getAPI(){
+    const response = await fetch(Joh);
     const data = await response.json();
-
-    const Temp = data.main.temp;
-    const Humidity = data.main.humidity;
+    const temp = data.main.temp;
+    const maxTemp = data.main.temp_max;
+    const minTemp =data.main.temp_min;
+    const feels =data.main.feels_like;
+    const humid = data.main.humidity;
+    const Wspeed = data.wind.speed;
+    let arrdesc = data.weather[0].description;
     
-
-    console.log(data);
-    document.getElementById("Temp").innerHTML = `
-    <h1>Here is the temp in your location</h1>
-    <h2>${Temp}</h2>
-    <h1>Here is the Humidity in your location</h1>
-    <h2>${Humidity}</h2>
+    
+    document.getElementById("JAPI").innerHTML = `
+    <ul>
+    <li>temp: ${temp}C</li>
+    <li>max: ${maxTemp}C</li>
+    <li>min: ${minTemp}C</li>
+    <li>feels like: ${feels}C</li>
+    <li>Humidity: ${humid}%</li>
+    <li>Wind speed: ${Wspeed}</li>
+    <li>Weather: ${arrdesc}</li>
+    </ul>
     `;
+
+
+};
+
+
+
+async function getAPI2(){
+    const response = await fetch(Cape);
+    const data = await response.json();
+    const temp = data.main.temp;
+    const maxTemp = data.main.temp_max;
+    const minTemp =data.main.temp_min;
+    const feels =data.main.feels_like;
+    const humid = data.main.humidity;
+    const Wspeed = data.wind.speed;
+    let arrdesc = data.weather[0].description;
+    
+    
+    document.getElementById("CAPI").innerHTML = `
+    <ul>
+    <li>temp: ${temp}C</li>
+    <li>max: ${maxTemp}C</li>
+    <li>min: ${minTemp}C</li>
+    <li>feels like: ${feels}C</li>
+    <li>Humidity: ${humid}%</li>
+    <li>Wind speed: ${Wspeed}</li>
+    <li>Weather: ${arrdesc}</li>
+    </ul>
+    `;
+
+
 }
-
-//console.log(navigator.geolocation.getCurrentPosition());
-
-
-
-
-//http:api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=
-
-
-// NOW http://api.openweathermap.org/data/2.5/weather?q=Vanderbijlpark&appid=2f5c5e465bbf3515e51929ac50b00522&units=metric
-
-// list of forecast https://api.openweathermap.org/data/2.5/forecast?q=Vanderbijlpark&appid=2f5c5e465bbf3515e51929ac50b00522&units=metric
